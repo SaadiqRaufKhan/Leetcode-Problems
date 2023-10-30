66c9aaea8f3107d36daa5e4e7bbe960e349411b6
Saadiq Rauf Khan <52396888+SaadiqRaufKhan@users.noreply.github.com>
2023-10-31 00:27:08 +0530

    Time: 7 ms (51.32%), Space: 13.9 MB (85.48%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,5 +1,63 @@
 class Solution {
 public:
+    int lowerBound(vector<int>& nums, int target) {
+        int s = 0;
+        int e = nums.size()-1;
+        
+        int ans = -1;
+        
+        while(s <= e) {
+            int mid = s + (e-s)/2;  // avoids integer overflow
+            if(nums[mid] == target) {
+                // don't return ans now and continue to search on the left side 
+                // update e to check for more occurence of target
+                ans = mid;
+                e = mid-1;  
+            }
+            else if(nums[mid] < target) {
+                s = mid + 1;
+            }
+            else {
+                e = mid - 1;
+            }
+        }
+        
+        return ans;
+    }
+    int upperBound(vector<int>& nums, int target) {
+        int s = 0;
+        int e = nums.size()-1;
+        
+        int ans = -1;
+        
+        while(s <= e) {
+            int mid = s + (e-s)/2;  // avoids integer overflow
+            if(nums[mid] == target) {
+               // don't return ans now and continue to search on the right side 
+                // update s to check for more occurence of target
+                ans = mid;
+                s = mid+1;  
+            }
+            else if(nums[mid] < target) {
+                s = mid + 1;
+            }
+            else {
+                e = mid - 1;
+            }
+        }
+        
+        return ans;
+    }
+    // Pure BS
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int lb = lowerBound(nums, target);
+        int ub = upperBound(nums, target);
+        return {lb, ub};
+    }
+    
+    
+    // BS + Linear
+    /*
     vector<int> searchRange(vector<int>& nums, int target) {
         vector<int> ans = {-1,-1};
 
@@ -28,5 +86,5 @@ public:
         }
 
         return ans;
-    }
+    }*/
 };
