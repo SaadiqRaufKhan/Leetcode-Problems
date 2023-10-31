76f68c99eb66f7e58693bf5db22fe9f0d27f796a
Saadiq Rauf Khan <52396888+SaadiqRaufKhan@users.noreply.github.com>
2023-10-31 12:45:05 +0530

    Time: 7 ms (94.01%), Space: 16.2 MB (67.36%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
+    // similar to finding ceiling
     char nextGreatestLetter(vector<char>& letters, char target) {
         int s = 0;
         int e = letters.size()-1;
         int ceil = -1;
         while(s <= e) {
             int mid = s + (e - s)/2;
-            cout << "mid = " << mid << endl;
-            if(letters[mid] - 'a' > target - 'a') {
+            if(letters[mid]  > target ) {
                 ceil = mid;
                 e = mid - 1;
             }
