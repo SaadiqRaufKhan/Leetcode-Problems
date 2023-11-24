5bd1521c2fb41250704b2f7e36dac492c772fc4c
Saadiq Rauf Khan <52396888+SaadiqRaufKhan@users.noreply.github.com>
2023-11-25 02:45:13 +0530

    Time: 147 ms (66.30%), Space: 51.5 MB (33.23%) - LeetHub

--- 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.cpp
+++ 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.cpp
@@ -7,16 +7,16 @@ public:
         map<int,int> count;
         map<int,int>::iterator it;
         int freq;
-        for(int &i:nums)		// Store the count of all numbers sorted.
+        for(int &i:nums)		
             count[i]++;
-        for(it=count.begin();it!=count.end();it++) {	// Start with the smallest number.
-            if(it->second) {   // If the count of smallest integer is non 0 check if next k numbers exist and have atleast same frequency.
+        for(it=count.begin();it!=count.end();it++) {	
+            if(it->second) {   
                 freq = it->second;
-                for(int i=0; i<k; i++)			// Checks for the next k-1 numbers.
-                    if(count[it->first+i]<freq) // We are unable to find ith consecutive number to the smallest(starting number) with atleast same frequency.
+                for(int i=0; i<k; i++)			
+                    if(count[it->first+i] < freq) 
                         return false;
                     else
-                        count[it->first+i]-=freq;   //Reduce the count of the numbers used.
+                        count[it->first+i] -= freq;   
             }
         }
         return true;
