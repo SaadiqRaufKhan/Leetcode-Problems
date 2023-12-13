fa7d8e495a3533ff2302f8bbaf38487382459c23
Saadiq Rauf Khan <52396888+SaadiqRaufKhan@users.noreply.github.com>
2023-12-13 15:52:29 +0530

    Time: 12 ms (53.78%), Space: 9.2 MB (67.69%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -7,21 +7,21 @@ public:
         vector<vector<int>> dp(n+1, vector<int>(m+1, 0)); // (n+1 x m+1)
         
         // base case: one of the strings is empty -> delete other completely
-        for(int j=0; j<=m; j++) {
-            dp[0][j] = j;
-        }
-        for(int i=0; i<=n; i++) {
-            dp[i][0] = i;
-        }
+ 
         
         // fill remaining dp (i & j represent the the length of s & t resp.)
-        for(int i=1; i<=n; i++) {
-            for(int j=1; j<=m; j++) {
+        for(int i=0; i<=n; i++) {
+            for(int j=0; j<=m; j++) {
+                if(i == 0) {
+                    dp[i][j] = j;
+                }
+                else if(j == 0) {
+                    dp[i][j] = i;
+                }
                 // ith and jth char match 
-                if(s[i-1] == t[j-1]) {
+                else if(s[i-1] == t[j-1]) {
                     dp[i][j] = dp[i-1][j-1];
                 }
-                
                 // ith and jth char don't match 
                 else {
                     int I = 1 + dp[i-1][j];   // insert
