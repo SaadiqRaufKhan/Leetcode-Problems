26c2bd5da19d035c8715904d937fd6007279e0d6
Saadiq Rauf Khan <52396888+SaadiqRaufKhan@users.noreply.github.com>
2023-12-20 14:12:16 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (60.97%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -2,35 +2,78 @@ class Solution {
 public:
 // alt approach could be to use stack 
     string reverseWords(string s) {
-        vector<vector<char>> words;
-        vector<char> w;
-        for(int i=0; i<s.length(); i++) {
-            if(s[i] == ' ' || i == s.length()-1) {
-                if(i == s.length()-1 && s[i] != ' ') {
-                    w.push_back(s[i]);
+        stack<string> st;
+        string word = "";
+        for(int i=0; i<s.size(); i++) {
+            char c = s[i];
+            if(c == ' ') {
+                if(word.size() != 0) {
+                    st.push(word);
+                    word = "";
                 }
-                if(w.size() > 0) {
-                    words.push_back(w);
-                    w.clear();
-                    continue;
-                }
-                continue;
             }
-            w.push_back(s[i]);
+            else {
+                word.push_back(c);
+            }
         }
-
-        // now we have all the words in the vector -> reverse as a whole, this will change the order of the words
-        reverse(words.begin(), words.end());
-
-        // pick each word, embed in a new string, add spaces and return that string
-        string ans;
-        for(auto word: words) {
-            for(auto ch: word) {
-                ans += ch;
-            }
+        if(word.size() != 0) {
+            st.push(word);
+            word = "";
+        }
+        string ans = "";
+        while(!st.empty()) {
+            ans += st.top();
             ans += " ";
+            st.pop();
         }
-        // taking substring to remove the " " at the end
-        return ans.substr(0, ans.size()-1);
+        ans.pop_back();
+        return ans;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//         vector<vector<char>> words;
+//         vector<char> w;
+//         for(int i=0; i<s.length(); i++) {
+//             if(s[i] == ' ' || i == s.length()-1) {
+//                 if(i == s.length()-1 && s[i] != ' ') {
+//                     w.push_back(s[i]);
+//                 }
+//                 if(w.size() > 0) {
+//                     words.push_back(w);
+//                     w.clear();
+//                     continue;
+//                 }
+//                 continue;
+//             }
+//             w.push_back(s[i]);
+//         }
+
+//         // now we have all the words in the vector -> reverse as a whole, this will change the order of the words
+//         reverse(words.begin(), words.end());
+
+//         // pick each word, embed in a new string, add spaces and return that string
+//         string ans;
+//         for(auto word: words) {
+//             for(auto ch: word) {
+//                 ans += ch;
+//             }
+//             ans += " ";
+//         }
+//         // taking substring to remove the " " at the end
+//         return ans.substr(0, ans.size()-1);
