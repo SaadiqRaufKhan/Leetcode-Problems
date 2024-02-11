0f68f87d626d30b7a52577c5b49feab77be9f677
Saadiq Rauf Khan <52396888+SaadiqRaufKhan@users.noreply.github.com>
2024-02-11 15:22:00 +0530

    Time: 16 ms (54.65%), Space: 15.5 MB (6.26%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,48 @@
+#include <unordered_set>
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+        unordered_set<int> uset;
+        
+        for(int i=0; i<nums.size(); i++) {
+            int pair = target - nums[i];
+            unordered_set<int> :: iterator itr = uset.find(pair);
+            // If we found a pair then get it's index and push the indices in the result vector
+            if(itr != uset.end()) {
+                int idx = 0;
+                while(nums[idx] != *itr) {
+                    idx++;
+                }
+                result.push_back(i);
+                result.push_back(idx);
+            }
+            // otherwise just insert into set
+            uset.insert(nums[i]);
+        }
+
+        return result;
+
+    }
+};
+
+
+
+
+ //        vector<int> result;
+//         unordered_set<int> uset;
+
+//         for(int i=0; i<nums.size(); i++) {
+//             int pair = target - nums[i];
+            
+//             if(uset.find(pair) != uset.end()) {
+//                 result.push_back(pair);
+//                 result.push_back(nums[i]);
+//             }
+
+//             uset.insert(nums[i]);
+//         }
+
+//         return result;
+
+
