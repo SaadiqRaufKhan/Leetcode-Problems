b4e3e03c79516c5c43fcf91d7e8dc3d232fdd943
Saadiq Rauf Khan <52396888+SaadiqRaufKhan@users.noreply.github.com>
2024-02-11 15:28:55 +0530

    Time: 114 ms (36.16%), Space: 12.5 MB (35.68%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,25 +3,35 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> result;
-        unordered_set<int> uset;
-        
-        for(int i=0; i<nums.size(); i++) {
-            int pair = target - nums[i];
-            unordered_set<int> :: iterator itr = uset.find(pair);
-            // If we found a pair then get it's index and push the indices in the result vector
-            if(itr != uset.end()) {
-                int idx = 0;
-                while(nums[idx] != *itr) {
-                    idx++;
+        for(int i=0; i<nums.size()-1; i++) {
+            for(int j=i+1; j<nums.size(); j++) {
+                if(nums[i] + nums[j] == target) {
+                    result.push_back(i);
+                    result.push_back(j);
+                    break;
                 }
-                result.push_back(i);
-                result.push_back(idx);
             }
-            // otherwise just insert into set
-            uset.insert(nums[i]);
         }
-
         return result;
+        // vector<int> result;
+        // unordered_set<int> uset;
+        
+        // for(int i=0; i<nums.size(); i++) {
+        //     int pair = target - nums[i];
+        //     unordered_set<int> :: iterator itr = uset.find(pair);
+        //     // If we found a pair then get it's index and push the indices in the result vector
+        //     if(itr != uset.end()) {
+        //         int idx = 0;
+        //         while(nums[idx] != *itr) {
+        //             idx++;
+        //         }
+        //         result.push_back(i);
+        //         result.push_back(idx);
+        //     }
+        //     // otherwise just insert into set
+        //     uset.insert(nums[i]);
+        // }
+        // return result;
 
     }
 };
