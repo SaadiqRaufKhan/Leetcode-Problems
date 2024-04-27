2b04291879f4b7e09899e69d4196bc303d7f273b
Saadiq Rauf Khan <52396888+SaadiqRaufKhan@users.noreply.github.com>
2024-04-27 17:37:31 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (98.65%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,16 +1,3 @@
-// class Solution {                      
-// public:
-//     int reverse(int x) {
-//         int r=0;      // decleare r 
-//         while(x){
-//          if (r>INT_MAX/10 || r<INT_MIN/10) return 0; // check 32 bit range if r is outside the range then return 0 
-//          r=r*10+x%10; // find remainder and add its to r
-//          x=x/10;     // Update the value of x
-//         } 
-//         return r;  // if r in the 32 bit range then return r
-//     }
-// };
-
 class Solution {
 public:
     int reverse(int x) {
@@ -19,8 +6,10 @@ public:
             if(newx > INT_MAX/10 || newx < INT_MIN/10) {
                 return 0;
             }
+            newx *= 10;
+            // newx = (newx * 10) + (x % 10);
+            newx += x % 10;
             
-            newx = (newx * 10) + (x % 10);
             x /= 10;
         }
         return newx;
