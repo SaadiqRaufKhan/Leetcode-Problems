b71fd0b3e26dc89798d4f93a63fe84645853d986
Saadiq Rauf Khan <52396888+SaadiqRaufKhan@users.noreply.github.com>
2024-10-03 21:22:55 +0530

    Time: 0 ms (100.00%), Space: 14 MB (52.65%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,58 +1,59 @@
 class Solution {
 public:
-    /*
-    int BS(vector<int> arr, int target, int s, int e) {        
-        while(s <= e) {
-            int mid = s + (e-s)/2;
-            if(arr[mid] == target) {
-                return mid;
-            }
-            else if(arr[mid] < target) {
-                s = mid + 1;
-            }
-            else {
-                e = mid - 1;
-            }
-        }
-        return -1;
-    }
-    // find the idx of rotation by finding the smallest element
-    int findSmallest(vector<int> arr, int s, int e) {
-        int n = arr.size();
-        
-        while(s <= e) {
-            int mid = s + (e-s)/2;
-            int next = (mid + 1) % n;
-            int prev = (mid - 1 + n) % n; // for handling mid = 0 
-            
-            if(arr[mid] < arr[prev] && arr[mid] <= arr[next]) {
-                return mid;
-            }
-            // check if the [0 to mid] half is sorted or not
-            else if(arr[0] <= arr[mid]) {
-                // smallest is in other half (right of mid)
-                s = mid + 1;
-            }
-            else {
-                e = mid - 1;
-            }
-        }
-        return 0;
-    }
-    int search(vector<int>& arr, int target) {
-        // this will give us the two sorted halves
-        int idx = findSmallest(arr, 0, arr.size()-1);
-        
-        // apply BS on both the sorted halves
-        int x = BS(arr, target, 0, idx-1);
-        int y = BS(arr, target, idx, arr.size()-1);
-        
-        if(x == -1 && y == -1) return -1;
-        
-        return (x == -1) ? y : x;
-    }
-    */
     
+//     int BS(vector<int> arr, int target, int s, int e) {        
+//         while(s <= e) {
+//             int mid = s + (e-s)/2;
+//             if(arr[mid] == target) {
+//                 return mid;
+//             }
+//             else if(arr[mid] < target) {
+//                 s = mid + 1;
+//             }
+//             else {
+//                 e = mid - 1;
+//             }
+//         }
+//         return -1;
+//     }
+//     // find the idx of rotation by finding the smallest element
+//     int findSmallest(vector<int> arr, int s, int e) {
+//         int n = arr.size();
+        
+//         while(s <= e) {
+//             int mid = s + (e-s)/2;
+//             int next = (mid + 1) % n;
+//             int prev = (mid - 1 + n) % n; // for handling mid = 0 
+            
+//             if(arr[mid] < arr[prev] && arr[mid] < arr[next]) {
+//                 return mid;
+//             }
+//             // check if the [0 to mid] half is sorted or not
+//             else if(arr[0] < arr[mid]) {
+//                 // smallest is in other half (right of mid)
+//                 s = mid + 1;
+//             }
+//             else {
+//                 e = mid - 1;
+//             }
+//         }
+//         return 0;
+//     }
+//     int search(vector<int>& arr, int target) {
+//         // this will give us the two sorted halves
+//         int idx = findSmallest(arr, 0, arr.size()-1);
+        
+//         // apply BS on both the sorted halves
+//         int x = BS(arr, target, 0, idx-1);
+//         int y = BS(arr, target, idx, arr.size()-1);
+        
+//         if(x == -1 && y == -1) return -1;
+        
+//         return (x == -1) ? y : x;
+//     }
+    
+    
+    // Coding minutes approach
     int search(vector<int>& arr, int target) {
         int s = 0;
         int e = arr.size() - 1;
@@ -75,9 +76,11 @@ public:
             }
             // Case 2: mid on line 2
             else {
+                // Case 2a: target on the right of mid
                 if(target>arr[mid] and target<=arr[e]){
                     s = mid + 1;
                 }
+                // Case 2b: target on the left of mid
                 else{
                     e = mid - 1;
                 }
