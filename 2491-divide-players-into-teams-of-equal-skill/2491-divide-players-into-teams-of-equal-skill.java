d1fed7ffb827bd752f4a3eba4cc554a41349baa9
Saadiq Rauf Khan <52396888+SaadiqRaufKhan@users.noreply.github.com>
2024-10-04 19:42:11 +0530

    Time: 14 ms (84.89%), Space: 57.9 MB (21.16%) - LeetHub

---
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.java
@@ -0,0 +1,49 @@
+// import java.util.Arrays;
+import java.util.Vector;
+class Solution {
+    
+    public long dividePlayers(int[] skill) {
+        int n = skill.length;
+        int teams = n/2;
+        int sum = 0;
+        for(int s: skill) {
+            sum += s;
+        }
+        
+        if(sum % teams != 0) return -1;
+        
+        int sumPerTeam = sum / teams;
+        
+        Arrays.sort(skill);
+        
+//         for(int s: skill) {
+//             System.out.println(s + " ");
+//         }
+        
+//         return 22;
+        int i = 0;
+        int j = n - 1;
+        Vector<Integer> vec = new Vector<Integer>();
+        while(i < j) {
+            if(skill[i] + skill[j] == sumPerTeam) {
+                vec.addElement(skill[i] * skill[j]);
+            }
+            else {
+                return -1;
+            }
+            i++; j--;
+        }
+        
+        long res = 0;
+        for(int v: vec) {
+            res += v;
+        }
+        return res;
+        
+    }
+}
+
+
+
+
+
