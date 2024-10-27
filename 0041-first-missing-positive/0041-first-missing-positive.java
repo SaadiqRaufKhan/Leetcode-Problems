83156f2285c44fda85f8545578433f5b67ac9b64
Saadiq Rauf Khan <52396888+SaadiqRaufKhan@users.noreply.github.com>
2024-10-27 21:28:46 +0530

    Time: 1 ms (100.00%), Space: 57.2 MB (55.92%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,26 @@
+class Solution {
+    // same approach as cycle sort
+    // ignore -ve and >n elements, sort the rest
+    // then check from the starting position of the array for 1 to n
+    public int firstMissingPositive(int[] nums) {
+        int i = 0;
+        int n = nums.length;
+        
+        while(i < n) {
+            int correctIdx = nums[i] - 1;
+            if(nums[i] <= 0 || nums[i] > n || nums[correctIdx] == nums[i]) {
+                i++;
+            }
+            else {
+                int temp = nums[i];
+                nums[i] = nums[correctIdx];
+                nums[correctIdx] = temp;
+            }
+        }
+        
+        for(int j=0; j<n; j++) {
+            if(nums[j] != j+1) return j+1;
+        }
+        return nums[n-1] + 1;
+    }
+}
