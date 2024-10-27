29e5122989d396cedf142d39027bb66b39d5144e
Saadiq Rauf Khan <52396888+SaadiqRaufKhan@users.noreply.github.com>
2024-10-27 23:25:40 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (40.59%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -4,21 +4,33 @@ public:
         if(n == 0) {
             return 1;
         }
-        long long power = n;
+        
+        long long power = n;    // copy n and make it +ve to deal with -ve powers
         if(n < 0) {
             power = -1 * power;
         }
         double ans = 1;
+        
         while(power > 0) {
-            if(power % 2 == 0) {   // if bit is 0, ans doesn't change
-                x = x * x;        // the base gets squared  
-                power = power / 2;
+            int last_bit = power & 1;
+            if(last_bit) {
+                ans *= x;
             }
-            else {              // if bit is 1, ans need to be multiplied
-                ans = ans * x;
-                power = power - 1;
-            }
+            x = x * x;          // square the base in each iteration
+            power = power>>1;   // right shift to remove the last bit of power
         }
+        
+        
+        // while(power > 0) {
+        //     if(power % 2 == 0) {   // if bit is 0, ans doesn't change
+        //         x = x * x;         // the base gets squared  
+        //         power = power / 2;
+        //     }
+        //     else {              // if bit is 1, ans need to be multiplied
+        //         ans = ans * x;
+        //         power = power - 1;
+        //     }
+        // }
 
         if(n < 0) {
             return (double)1.0 / (double)ans;
