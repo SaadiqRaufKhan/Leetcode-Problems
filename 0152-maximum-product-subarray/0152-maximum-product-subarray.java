408924dafbe14c882a78feb663ad84344c6b264b
Saadiq Rauf Khan <52396888+SaadiqRaufKhan@users.noreply.github.com>
2024-11-23 13:13:52 +0530

    Time: 3 ms (16.43%), Space: 45 MB (34.19%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,17 +1,16 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int max_till_now = nums[0];
-        int min_till_now = nums[0];
-        int res = nums[0];
+        int[] max_till_now = nums.clone();
+        int[] min_till_now = nums.clone();
+        int[] res = nums.clone();
         
         for(int i=1; i<nums.length; i++) {
-            int temp = Math.min(nums[i], Math.min(nums[i]*min_till_now, nums[i]*max_till_now));
-            max_till_now = Math.max(nums[i], Math.max(nums[i]*min_till_now, nums[i]*max_till_now));
-            min_till_now = temp;
+            min_till_now[i] = Math.min(nums[i], Math.min(nums[i]*min_till_now[i-1], nums[i]*max_till_now[i-1]));
+            max_till_now[i] = Math.max(nums[i], Math.max(nums[i]*min_till_now[i-1], nums[i]*max_till_now[i-1]));
             
-            res = Math.max(res, max_till_now);
+            res[i] = Math.max(res[i-1], max_till_now[i]);
             
         }
-        return res;
+        return res[nums.length - 1];
     }
 }
