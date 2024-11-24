1ee98d97930587afb31a4c4ceb6cd12a2332a0f0
Saadiq Rauf Khan <52396888+SaadiqRaufKhan@users.noreply.github.com>
2024-11-24 15:53:47 +0530

    Time: 20 ms (77.51%), Space: 51.3 MB (11.35%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -4,32 +4,50 @@ class Solution {
         int l2 = text2.length();
         
         // initialize dp
-        ArrayList<ArrayList<Integer>> dp = new ArrayList<>(l1+1);
-        for(int i=0; i<=l1; i++) {
-            ArrayList<Integer> temp = new ArrayList<>(l2+1);
-            for(int j=0; j<=l2; j++) {
-                temp.add(0);
-            }
-            dp.add(temp);
-        }
+        int[][] dp = new int[l1+1][l2+1];
         
         // fill dp
         for(int i=1; i<=l1; i++) {
             for(int j=1; j<=l2; j++) {
                 if(text1.charAt(i-1) == text2.charAt(j-1)) {
-                    int res = dp.get(i-1).get(j-1) + 1;
-                    dp.get(i).set(j, res);
+                    dp[i][j] = dp[i-1][j-1] + 1;
                 }
                 else {
-                    int res = Math.max(dp.get(i-1).get(j), dp.get(i).get(j-1));
-                    dp.get(i).set(j, res);
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
                 }
             }
         }
         
-        return dp.get(l1).get(l2);
-        
-        
-        
+        return dp[l1][l2];     
     }
+//     public int longestCommonSubsequence(String text1, String text2) {
+//         int l1 = text1.length();
+//         int l2 = text2.length();
+        
+//         // initialize dp
+//         ArrayList<ArrayList<Integer>> dp = new ArrayList<>(l1+1);
+//         for(int i=0; i<=l1; i++) {
+//             ArrayList<Integer> temp = new ArrayList<>(l2+1);
+//             for(int j=0; j<=l2; j++) {
+//                 temp.add(0);
+//             }
+//             dp.add(temp);
+//         }
+        
+//         // fill dp
+//         for(int i=1; i<=l1; i++) {
+//             for(int j=1; j<=l2; j++) {
+//                 if(text1.charAt(i-1) == text2.charAt(j-1)) {
+//                     int res = dp.get(i-1).get(j-1) + 1;
+//                     dp.get(i).set(j, res);
+//                 }
+//                 else {
+//                     int res = Math.max(dp.get(i-1).get(j), dp.get(i).get(j-1));
+//                     dp.get(i).set(j, res);
+//                 }
+//             }
+//         }
+        
+//         return dp.get(l1).get(l2);        
+//     }
 }
