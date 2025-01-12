0dde42b7f22e819e5f1f25f5d7e6ad9ed6c83d69
Saadiq Rauf Khan <52396888+SaadiqRaufKhan@users.noreply.github.com>
2025-01-12 14:38:40 +0530

    0056-MERGE-INTERVALS Stats: Time: 8 ms (86.92%), Space: 46.9 MB (22.8%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -2,22 +2,40 @@ class Solution {
     public int[][] merge(int[][] intervals) {
         int n = intervals.length;
         Arrays.sort(intervals, (row1, row2) -> Integer.compare(row1[0], row2[0]));
-        for(int i=0; i<n; i++) {
-            System.out.println(Arrays.toString(intervals[i]));
-        }
+
         List<int[]> ans = new ArrayList<>();
-        int i = 0;
-        while(i < n) {
-            int left = intervals[i][0];
-            int right = intervals[i][1];
-            while(i < n-1 && right >= intervals[i+1][0]) {
-                right = Math.max(right, intervals[i+1][1]);
-                i++;
+        for(int i=0; i<n; i++) {
+            int size = ans.size();
+            if(size == 0) {
+                ans.add(intervals[i]);
             }
-            ans.add(new int[]{left, right});
-            i++;
+            else if((ans.get(size-1))[1] < intervals[i][0]) {
+                ans.add(intervals[i]);
+            }
+            else {
+                (ans.get(size-1))[1] = Math.max(intervals[i][1], (ans.get(size-1))[1]);
+            }
         }
-        int[][] result = ans.toArray(new int[0][]);
-        return result;
+        return ans.toArray(new int[0][]);
     }
 }
+
+    // public int[][] merge(int[][] intervals) {
+    //     int n = intervals.length;
+    //     Arrays.sort(intervals, (row1, row2) -> Integer.compare(row1[0], row2[0]));
+
+    //     List<int[]> ans = new ArrayList<>();
+    //     int i = 0;
+    //     while(i < n) {
+    //         int left = intervals[i][0];
+    //         int right = intervals[i][1];
+    //         while(i < n-1 && right >= intervals[i+1][0]) {
+    //             right = Math.max(right, intervals[i+1][1]);
+    //             i++;
+    //         }
+    //         ans.add(new int[]{left, right});
+    //         i++;
+    //     }
+    //     int[][] result = ans.toArray(new int[0][]);
+    //     return result;
+    // }
