a633ecc10a115d4aaac2fdfbae6f56e5890442be
Saadiq Rauf Khan <52396888+SaadiqRaufKhan@users.noreply.github.com>
2025-01-12 14:39:49 +0530

    0056-MERGE-INTERVALS Stats: Time: 8 ms (86.92%), Space: 46.6 MB (63.63%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -6,14 +6,11 @@ class Solution {
         List<int[]> ans = new ArrayList<>();
         for(int i=0; i<n; i++) {
             int size = ans.size();
-            if(size == 0) {
-                ans.add(intervals[i]);
-            }
-            else if((ans.get(size-1))[1] < intervals[i][0]) {
+            if(size == 0 || ans.get(size-1)[1] < intervals[i][0]) {
                 ans.add(intervals[i]);
             }
             else {
-                (ans.get(size-1))[1] = Math.max(intervals[i][1], (ans.get(size-1))[1]);
+                ans.get(size-1)[1] = Math.max(intervals[i][1], ans.get(size-1)[1]);
             }
         }
         return ans.toArray(new int[0][]);
