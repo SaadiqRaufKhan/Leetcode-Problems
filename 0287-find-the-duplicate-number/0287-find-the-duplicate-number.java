18273ac53de5065123f4256a975a0365a9e4c56c
Saadiq Rauf Khan <52396888+SaadiqRaufKhan@users.noreply.github.com>
2025-01-17 22:17:06 +0530

    0287-FIND-THE-DUPLICATE-NUMBER Stats: Time: 6 ms (50.7%), Space: 58.3 MB (47.27%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,5 +1,53 @@
 class Solution {
     public int findDuplicate(int[] nums) {
+        // apply count sort on the array
+        // whichever element is not at its correct index is the ans
+        int i = 0;
+        while(i < nums.length) {
+            int correctIdx = nums[i] - 1;
+            // the element at correctIdx should be correctIdx+1
+            if(nums[correctIdx] != nums[i]) {
+                int temp = nums[correctIdx];
+                nums[correctIdx] = nums[i];
+                nums[i] = temp;
+            }
+            else {
+                i++;
+            }
+
+        }
+        for(int j=0; j<nums.length; j++) {
+            if(nums[j] != j+1) return nums[j];
+        }
+        return -1;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
         int i = 0;
         int n = nums.length;
         
@@ -19,5 +67,5 @@ class Solution {
             if(nums[j] != j+1) return nums[j];
         }
         return -1;
-    }
-}
+
+ */
