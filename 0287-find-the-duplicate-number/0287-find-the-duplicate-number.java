548ad53d09efb3750eba4aab0adc641bf844017d
Saadiq Rauf Khan <52396888+SaadiqRaufKhan@users.noreply.github.com>
2025-01-17 22:18:34 +0530

    0287-FIND-THE-DUPLICATE-NUMBER Stats: Time: 4 ms (88.32%), Space: 58.1 MB (64.64%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -2,24 +2,37 @@ class Solution {
     public int findDuplicate(int[] nums) {
         // apply count sort on the array
         // whichever element is not at its correct index is the ans
-        int i = 0;
-        while(i < nums.length) {
-            int correctIdx = nums[i] - 1;
-            // the element at correctIdx should be correctIdx+1
-            if(nums[correctIdx] != nums[i]) {
-                int temp = nums[correctIdx];
-                nums[correctIdx] = nums[i];
-                nums[i] = temp;
-            }
-            else {
-                i++;
-            }
+        // int i = 0;
+        // while(i < nums.length) {
+        //     int correctIdx = nums[i] - 1;
+        //     // the element at correctIdx should be correctIdx+1
+        //     if(nums[correctIdx] != nums[i]) {
+        //         int temp = nums[correctIdx];
+        //         nums[correctIdx] = nums[i];
+        //         nums[i] = temp;
+        //     }
+        //     else {
+        //         i++;
+        //     }
 
+        // }
+        // for(int j=0; j<nums.length; j++) {
+        //     if(nums[j] != j+1) return nums[j];
+        // }
+        // return -1;
+        int slow = nums[0];
+        int fast = nums[0];
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        fast = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        for(int j=0; j<nums.length; j++) {
-            if(nums[j] != j+1) return nums[j];
-        }
-        return -1;
+        return slow;
     }
 }
 
