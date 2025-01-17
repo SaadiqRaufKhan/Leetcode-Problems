46bec3509464575d271ef9c90822aab46c223039
Saadiq Rauf Khan <52396888+SaadiqRaufKhan@users.noreply.github.com>
2025-01-17 22:19:53 +0530

    0287-FIND-THE-DUPLICATE-NUMBER Stats: Time: 6 ms (50.7%), Space: 58.1 MB (71.28%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -2,83 +2,37 @@ class Solution {
     public int findDuplicate(int[] nums) {
         // apply count sort on the array
         // whichever element is not at its correct index is the ans
-        // int i = 0;
-        // while(i < nums.length) {
-        //     int correctIdx = nums[i] - 1;
-        //     // the element at correctIdx should be correctIdx+1
-        //     if(nums[correctIdx] != nums[i]) {
-        //         int temp = nums[correctIdx];
-        //         nums[correctIdx] = nums[i];
-        //         nums[i] = temp;
-        //     }
-        //     else {
-        //         i++;
-        //     }
-
-        // }
-        // for(int j=0; j<nums.length; j++) {
-        //     if(nums[j] != j+1) return nums[j];
-        // }
-        // return -1;
-        int slow = nums[0];
-        int fast = nums[0];
-        do {
-            slow = nums[slow];
-            fast = nums[nums[fast]];
-        } while (slow != fast);
-
-        fast = nums[0];
-        while (slow != fast) {
-            slow = nums[slow];
-            fast = nums[fast];
-        }
-        return slow;
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
         int i = 0;
-        int n = nums.length;
-        
-        while(i < n) {
-            int correctIdx = nums[i] - 1;   // nums[i]'s correct idx
-            if(nums[correctIdx] != nums[i]) {   // swap 
-                int temp = nums[i];
-                nums[i] = nums[correctIdx];
-                nums[correctIdx] = temp;
+        while(i < nums.length) {
+            int correctIdx = nums[i];
+            // the element at correctIdx should be correctIdx+1
+            if(nums[correctIdx] != nums[i]) {
+                int temp = nums[correctIdx];
+                nums[correctIdx] = nums[i];
+                nums[i] = temp;
             }
             else {
                 i++;
             }
+
         }
-        
-        for(int j=0; j<n; j++) {
+        for(int j=0; j<nums.length; j++) {
             if(nums[j] != j+1) return nums[j];
         }
         return -1;
 
- */
+        // int slow = nums[0];
+        // int fast = nums[0];
+        // do {
+        //     slow = nums[slow];
+        //     fast = nums[nums[fast]];
+        // } while (slow != fast);
+
+        // fast = nums[0];
+        // while (slow != fast) {
+        //     slow = nums[slow];
+        //     fast = nums[fast];
+        // }
+        // return slow;
+    }
+}
