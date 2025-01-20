72a91cc2f0d55e996701210e057c6219edbb1b25
Saadiq Rauf Khan <52396888+SaadiqRaufKhan@users.noreply.github.com>
2025-01-20 22:31:37 +0530

    1646-KTH-MISSING-POSITIVE-NUMBER Stats: Time: 1 ms (24.02%), Space: 43 MB (34.44%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.java
@@ -3,11 +3,11 @@ class Solution {
         int i = 0;
         int ans = 1;
 
-        while(i < arr.length && ans == arr[i]) {
-            ans++;
-            i++;
-        }
-        if(k == 1) return ans;
+        // while(i < arr.length && ans == arr[i]) {
+        //     ans++;
+        //     i++;
+        // }
+        // if(k == 1) return ans;
 
 
         while(i < arr.length && k > 1) {
@@ -22,7 +22,10 @@ class Solution {
                 }
             }
         }
+        // we have used all k's
         if(k == 1) {
+            // condition to check if ans matches with arr[i]
+            // increase ans while it is same as arr[i]
             while(i < arr.length && ans == arr[i]) {
                 ans++;
                 i++;
@@ -30,6 +33,7 @@ class Solution {
             return ans;
         }
 
+        // array was exhausted but k was still not 1
         while(k > 1) {
             ans++;
             k--;
