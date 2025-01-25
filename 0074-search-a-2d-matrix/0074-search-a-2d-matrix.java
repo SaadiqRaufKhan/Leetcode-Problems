19d09c837115228547be71a00e6ddf48760ad15a
Saadiq Rauf Khan <52396888+SaadiqRaufKhan@users.noreply.github.com>
2025-01-25 13:23:01 +0530

    0074-SEARCH-A-2D-MATRIX Stats: Time: 0 ms (100%), Space: 42.4 MB (28.41%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,9 +1,9 @@
 class Solution {
-    int binarySearch(int[] arr, int s, int e, int target) {
+    boolean binarySearch(int[] arr, int s, int e, int target) {
         while(s <= e) {
             int mid = s + (e - s)/2;
             if(target == arr[mid]) {
-                return mid;
+                return true;
             }
             else if(target < arr[mid]) {
                 e = mid - 1;
@@ -12,17 +12,18 @@ class Solution {
                 s = mid + 1;
             }
         }
-        return -1;
+        return false;
     }
 
 
     // find the row using binary search logic
-    int findRow(int[][] matrix, int s, int e, int target) {
+    boolean binarySearch2D(int[][] matrix, int s, int e, int target) {
         int n = matrix[0].length;
         while(s <= e) {
             int mid = s + (e - s)/2;
             if(target >= matrix[mid][0] && target <= matrix[mid][n-1]) {
-                return mid;
+                // now find in this row
+                return binarySearch(matrix[mid], 0, n-1, target);
             }
             else if (target < matrix[mid][0]) {
                 e = mid - 1;
@@ -31,7 +32,7 @@ class Solution {
                 s = mid + 1;
             }
         }
-        return -1;
+        return false;
     }
 
     public boolean searchMatrix(int[][] matrix, int target) {
@@ -39,21 +40,7 @@ class Solution {
         int m = matrix.length;      // rows
         int n = matrix[0].length;   // cols
 
-        // System.
-
-        int row = findRow(matrix, 0, m-1, target);
-
-        System.out.println("row = " + row);
-
-        if(row == -1) return false;
-
-        int index = binarySearch(matrix[row], 0, matrix[row].length-1, target);
-
-        System.out.println("index = " + index);
-
-        if(index == -1) return false;
-
-        return true;
+        return binarySearch2D(matrix, 0, m-1, target);
 
         
     }
