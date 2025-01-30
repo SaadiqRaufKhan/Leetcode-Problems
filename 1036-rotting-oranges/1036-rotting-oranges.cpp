51c829e9a5365a58770762c260ac9b70be3d284a
Saadiq Rauf Khan <52396888+SaadiqRaufKhan@users.noreply.github.com>
2025-01-30 22:18:56 +0530

    1036-ROTTING-ORANGES Stats: Time: 0 ms (100%), Space: 17.1 MB (41.82%) - LeetHub

---
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        // copy the grid in another matrix 
+        vector<vector<int>> visited = grid;
+
+        // making queue in which we will fill rotten oranges
+        queue<pair<int,int>> q;
+        
+        // count the total no. fresh oranges at the beginning
+        // and push all rotten oranges in the queue
+        int freshCount = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (visited[i][j] == 2) {
+                    q.push({i, j});
+                }
+                if (visited[i][j] == 1) {
+                    freshCount++;
+                }
+            }
+        }
+        
+        // freshCount = 0 means that there are no fresh oranges to be rotten -> we can simply return 0 min
+        if (freshCount == 0)
+            return 0;
+
+        int minutes = -1;
+        vector<pair<int, int>> dirs = {{1, 0},{-1, 0},{0, -1},{0, 1}};
+        while (!q.empty()) {
+            // pop all rotten oranges and make their neighbours as rotten
+            int size = q.size();
+            while (size--) {
+                auto [x, y] = q.front();
+                q.pop();
+                for (auto [dx, dy] : dirs) {
+                    int i = x + dx;
+                    int j = y + dy;
+                    if (i >= 0 && i < m && j >= 0 && j < n && visited[i][j] == 1) {
+                        visited[i][j] = 2;
+                        freshCount--;
+                        q.push({i, j});
+                    }
+                }
+            }
+            minutes++;
+        }
+        
+        if (freshCount == 0)
+            return minutes;
+        return -1;
+    }
+};
