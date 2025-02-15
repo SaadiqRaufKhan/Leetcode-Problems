453579111c087f13bb8574c3785fa9d38a81d358
Saadiq Rauf Khan <52396888+SaadiqRaufKhan@users.noreply.github.com>
2025-02-15 19:07:05 +0530

    0082-REMOVE-DUPLICATES-FROM-SORTED-LIST-II Stats: Time: 0 ms (100%), Space: 15.8 MB (41.5%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.cpp
@@ -11,53 +11,89 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        // corner case
-        if(head == NULL || head->next == NULL) return head;
+        // // corner case
+        // if(head == NULL || head->next == NULL) return head;
 
-        ListNode* prev = new ListNode(0);
-        prev->next = head;
-        ListNode* curr = head;
-        bool flag = false;
+        // // add an extra dummy node before the head and call it prev
+        // // this will avoid the overhead of computing prev
+        // ListNode* prev = new ListNode(0);
+        // prev->next = head;
+        // ListNode* curr = head;
+        // bool flag = false;
 
-        while(curr->next != NULL) {
-            if(curr->val == curr->next->val) {
-                flag = true;
-                ListNode* duplicate = curr->next;
-                curr->next = duplicate->next;
-                delete duplicate;
-            }
-            else {
-                // this means that the curr node also needs to be deleted
-                if(flag) {
-                    flag = false;
-                    // check if the curr node is head node or note
-                    if(curr == head) {
-                        head = curr->next;
-                        delete curr;
-                        curr = head;
-                    } 
-                    else {
-                        prev->next = curr->next;
-                        delete curr;
-                        curr = prev->next;
-                    }
-                }
-                else {
-                    prev = curr;
-                    curr = curr->next;
-                }
-            }
+        // while(curr->next != NULL) {
+        //     if(curr->val == curr->next->val) {
+        //         flag = true;
+        //         ListNode* duplicate = curr->next;
+        //         curr->next = duplicate->next;
+        //         delete duplicate;
+        //     }
+        //     else {
+        //         // this means that the curr node also needs to be deleted
+        //         if(flag) {
+        //             flag = false;
+        //             // check if the curr node is head node or note
+        //             if(curr == head) {
+        //                 head = curr->next;
+        //                 delete curr;
+        //                 curr = head;
+        //             } 
+        //             else {
+        //                 prev->next = curr->next;
+        //                 delete curr;
+        //                 curr = prev->next;
+        //             }
+        //         }
+        //         else {
+        //             prev = curr;
+        //             curr = curr->next;
+        //         }
+        //     }
+        // }
+        // if(flag) {
+        //     if(curr == head) {
+        //         head = NULL;
+        //         delete curr;
+        //     }
+        //     else {
+        //         prev->next = NULL;
+        //         delete curr;
+        //     }
+        // }
+        // return head;
+            if (!head || !head->next) return head;
+
+    ListNode* dummy = new ListNode(0); // Dummy node before head
+    dummy->next = head;
+    ListNode* prev = dummy; // Previous confirmed unique node
+    ListNode* curr = head;  // Current node to check
+
+    while (curr) {
+        bool isDuplicate = false;
+        
+        // Skip all nodes with the same value
+        while (curr->next && curr->val == curr->next->val) {
+            isDuplicate = true;
+            ListNode* temp = curr;
+            curr = curr->next;
+            delete temp; // Free memory of duplicate node
         }
-        if(flag) {
-            if(curr == head) {
-                head = NULL;
-                delete curr;
-            }
-            else {
-                prev->next = NULL;
-                delete curr;
-            }
+
+        if (isDuplicate) {
+            // Remove duplicates by skipping them
+            prev->next = curr->next;
+            delete curr; // Free memory of the last duplicate node
+        } else {
+            // Move prev forward if no duplicate was found
+            prev = prev->next;
         }
-        return head;
+        
+        // Move curr forward
+        curr = prev->next;
+    }
+
+    ListNode* newHead = dummy->next;
+    delete dummy; // Free dummy node memory
+    return newHead;
     }
 };
