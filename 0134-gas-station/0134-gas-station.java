ebd1495f0d3c9d35727671f3e50dbbf1639c8c5c
Saadiq Rauf Khan <52396888+SaadiqRaufKhan@users.noreply.github.com>
2025-02-15 14:44:21 +0530

    0134-GAS-STATION Stats: Time: 3 ms (38.32%), Space: 56.7 MB (16.12%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,38 +1,11 @@
 class Solution {
-    
-    boolean isPossible(int pos, int[] gas, int[] cost) {
-        int n = gas.length;
-        int i = 0;
-        int gassum = 0;
-        int costsum = 0;
-        while(i < n) {
-            gassum += gas[pos];
-            costsum += cost[pos];
-            if(gassum >= costsum) {
-                pos = (pos + 1) % n;
-                i++;
-            }
-            else {
-                return false;
-            }
-        }
-        return true;
-    }
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        // int n = gas.length;
-        // for(int i=0; i<n; i++) {
-        //     if(gas[i] >= cost[i]) {
-        //         if(isPossible(i, gas, cost)) {
-        //             return i;
-        //         }
-        //     }
-        // }
-        // return -1;
-        int totalGas = 0, currentGas = 0, startIndex = 0;
+        int totalGas = 0, totalCost = 0, currentGas = 0, startIndex = 0;
 
         for (int i = 0; i < gas.length; i++) {
             int netGain = gas[i] - cost[i];
-            totalGas += netGain;
+            totalGas += gas[i];
+            totalCost += cost[i];
             currentGas += netGain;
 
             // If currentGas drops below 0, reset the start index
@@ -43,7 +16,7 @@ class Solution {
         }
 
         // If total gas available is less than total cost, return -1 (not possible)
-        return totalGas >= 0 ? startIndex : -1;
+        return (totalGas - totalCost) >= 0 ? startIndex : -1;
 
     }
 }
