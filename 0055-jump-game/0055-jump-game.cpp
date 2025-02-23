dca67a9711d9ec130ab29732805bfeee2d91c669
Saadiq Rauf Khan <52396888+SaadiqRaufKhan@users.noreply.github.com>
2025-02-23 22:56:41 +0530

    0055-JUMP-GAME Stats: Time: 2 ms (30.72%), Space: 52.1 MB (98.67%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int n = nums.size();
-        vector<bool> res(n, false);
-        res[n-1] = true;
-        for(int i=n-2; i>=0; i--) {
-            for(int j=1; j<=nums[i] && i + j < n; j++) {
-                if(res[i+j]) {
-                    res[i] = true;
-                    break;
-                }
+        int dist = 0;
+        for(int i=0; i<=dist; i++) {
+            dist = max(dist, i+nums[i]);    // the furthest that 'i' can go
+            if(dist >= nums.size() - 1) {
+                return true;
             }
         }
-
-        return res[0];
+        return false;
     }
 };
+
+/*
+    int n = nums.size();
+    vector<bool> res(n, false);
+    res[n-1] = true;
+    for(int i=n-2; i>=0; i--) {
+        for(int j=1; j<=nums[i] && i + j < n; j++) {
+            if(res[i+j]) {
+                res[i] = true;
+                break;
+            }
+        }
+    }
+
+    return res[0];
+*/
