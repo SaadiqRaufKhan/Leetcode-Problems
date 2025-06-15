60991ee978f079df8f9d78798f77a544ad2670a5
Saadiq Rauf Khan <52396888+SaadiqRaufKhan@users.noreply.github.com>
2025-06-15 18:24:36 +0530

    0091-DECODE-WAYS Stats: Time: 1 ms (83.69%), Space: 41.7 MB (77.26%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -8,29 +8,30 @@ class Solution {
 
         int[] dp = new int[n];
 
-        int d1 = decode(s.charAt(n-1));
-        int d2 = decode(s.charAt(n-2));
+        int d1 = decode(s.charAt(n-1));     // 1s place
+        int d10 = decode(s.charAt(n-2));    // 10s place
         // initialize the last two cells
         if(d1 == 0) {
             dp[n-1] = 0;
-            if(d2 == 0 || d2 >= 3) dp[n-2] = 0;
-            else if(d2 == 2 || d2 == 1) dp[n-2] = 1;
+            if(d10 == 0 || d10 >= 3) dp[n-2] = 0;
+            else if(d10 == 2 || d10 == 1) dp[n-2] = 1;
         }
         else {
             dp[n-1] = 1;
-            if(d2 == 0) dp[n-2] = 0;
+            if(d10 == 0) dp[n-2] = 0;
             else {
-                if(d2*10 + d1 <= 26) dp[n-2] = 2;
+                if(d10*10 + d1 <= 26) dp[n-2] = 2;
                 else dp[n-2] = 1;
             }
         }
 
-        // fill the remaining dp array
+        // fill the remaining dp array going right to left, dp[0] will contain the final answer
+        // logic can be understood by drawing the dp array and running examples
         for(int i=n-3; i>=0; i--) {
-            d1 = decode(s.charAt(i+1));
-            d2 = decode(s.charAt(i));
-            int sum = d2*10 + d1;
-            if(d2 == 0) {
+            d1 = decode(s.charAt(i+1)); // 1s place
+            d10 = decode(s.charAt(i));  // 10s place
+            int sum = d10*10 + d1;
+            if(d10 == 0) {
                 dp[i] = 0;
             }
             else if(d1 == 0) {
