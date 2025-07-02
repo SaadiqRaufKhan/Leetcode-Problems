c1ff6d4a64350f67c5e0bde37e13cf684f8bcdac
Saadiq Rauf Khan <52396888+SaadiqRaufKhan@users.noreply.github.com>
2025-07-02 20:50:28 +0530

    0062-UNIQUE-PATHS Stats: Time: 0 ms (100%), Space: 7.7 MB (96.56%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        // similar to total no of ways to arrange downs and rights
+        int N = n + m - 2;
+        int r = m - 1; 
+        double res = 1;
+        
+        for (int i = 1; i <= r; i++)
+            res = res * (N - r + i) / i;
+        return (int)res;
+    }
+};
