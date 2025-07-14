004f474c9377fd041bb4b1856ad95abdfd1c0911
Saadiq Rauf Khan <52396888+SaadiqRaufKhan@users.noreply.github.com>
2025-07-14 21:02:22 +0530

    0188-BEST-TIME-TO-BUY-AND-SELL-STOCK-IV Stats: Time: 250 ms (5%), Space: 16.1 MB (67.31%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -2,43 +2,45 @@
 class Solution {
 public:
     
-    int recursiveHelper(vector<int> prices, int idx, int k) {
-        // k reaches 0 or one or no stock left
-        if(k == 0 || idx >= prices.size()-1)
-            return 0;
+    // int recursiveHelper(vector<int> prices, int idx, int k) {
+    //     // k reaches 0 or one or no stock left
+    //     if(k == 0 || idx >= prices.size()-1)
+    //         return 0;
         
-        // don't buy
-        int x = recursiveHelper(prices, idx+1, k);
+    //     // don't buy
+    //     int x = recursiveHelper(prices, idx+1, k);
         
-        // buy
-        int y = 0;
-        for(int i=idx+1; i<prices.size(); i++) {
-            if(prices[i] > prices[idx]) {
-                int currProfit = prices[i] - prices[idx];
-                y = max(y, currProfit + recursiveHelper(prices, i+1, k-1));
-            }
-        }
+    //     // buy
+    //     int y = 0;
+    //     for(int i=idx+1; i<prices.size(); i++) {
+    //         if(prices[i] > prices[idx]) {
+    //             int currProfit = prices[i] - prices[idx];
+    //             y = max(y, currProfit + recursiveHelper(prices, i+1, k-1));
+    //         }
+    //     }
         
-        return max(x, y);
+    //     return max(x, y);
         
-    }
+    // }
     int maxProfit(int k, vector<int>& prices) {
         // recursive
         // int ans = recursiveHelper(prices, 0, k);
         // return ans;
         
         // DP
-        vector<vector<int>> dp(k+1, vector<int>(prices.size()+1, 0));
+        int n = prices.size();
+        vector<vector<int>> dp(k+1, vector<int>(n+1, 0));
         
         // fill dp: right to left and up to down
         for(int i=1; i<=k; i++) {
-            for(int j=prices.size()-2; j>=0; j--) {
+            // start j from 2nd last element so that we have at least two 2 days for buying and selling
+            for(int j=n-2; j>=0; j--) {
                 // not buy curr stock
                 int x = dp[i][j+1];
                 
                 // buy curr stock
                 int y = 0;
-                for(int l=j+1; l<prices.size(); l++) {
+                for(int l=j+1; l<n; l++) {
                     if(prices[l] > prices[j]) {
                         int currProf = prices[l] - prices[j];
                         y = max(y, currProf + dp[i-1][l+1]);
