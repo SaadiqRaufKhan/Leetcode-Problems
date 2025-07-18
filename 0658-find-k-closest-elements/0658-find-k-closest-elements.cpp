1c1ad7ded4193688eef62b9c44d80a7b64c7e07f
Saadiq Rauf Khan <52396888+SaadiqRaufKhan@users.noreply.github.com>
2025-07-18 23:52:06 +0530

    0658-FIND-K-CLOSEST-ELEMENTS Stats: Time: 33 ms (12.71%), Space: 38.8 MB (22.93%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -4,17 +4,11 @@ public:
         priority_queue<pair<int,int>> pq;
 
         for(int i=0; i<arr.size(); i++) {
-            pair<int, int> p = {abs(arr[i] - x), arr[i]};
-            if(pq.size() < k) {
-                pq.push(p);
+            pq.push({abs(arr[i] - x), arr[i]});
+            if(pq.size() > k) {
+                pq.pop();
             }
-            else {
-                pair<int,int> top = pq.top();
-                if(p < top) {
-                    pq.pop();
-                    pq.push(p);
-                }
-            }
+            
         }
 
         vector<int> ans;
