32e9bb835f635cd0bb5a536f58674b4f37388505
Saadiq Rauf Khan <52396888+SaadiqRaufKhan@users.noreply.github.com>
2025-08-31 11:50:04 +0530

    0945-SNAKES-AND-LADDERS Stats: Time: 12 ms (16.2%), Space: 44.8 MB (51.44%) - LeetHub

--- 0945-snakes-and-ladders/0945-snakes-and-ladders.java
+++ 0945-snakes-and-ladders/0945-snakes-and-ladders.java
@@ -1,10 +1,18 @@
 class Solution {
     public int snakesAndLadders(int[][] board) {
         int n = board.length;
-        int cell = (board[n-1][0] == -1) ? 1 : board[n-1][0];
-        boolean dir = true; // true: l->r, false: r->l
+        int target = n*n;
+
+        // check if there is a ladder on the first cell itself or not
+        // int cell = (board[n-1][0] == -1) ? 1 : board[n-1][0];
+        int cell = 1;
+
+        // store the cells with all the snakes and ladders
         Map<Integer, Integer> map = new HashMap<>();
+
+        boolean dir = true; // true: l->r, false: r->l
         for(int i=n-1; i>=0; i--) {
+            if(cell == target) break;
             if(dir) {   // move: l->r
                 for(int j=0; j<n; j++) {
                     if(board[i][j] != -1) map.put(cell, board[i][j]);
@@ -21,19 +29,17 @@ class Solution {
             }
         }
 
-        // System.out.println(map);
         Set<Integer> visited = new HashSet<>();
-        int target = n * n;
-        int curr = (board[n-1][0] == -1) ? 1 : board[n-1][0];
+        // int start = (board[n-1][0] == -1) ? 1 : board[n-1][0];
+        int start = 1;
         Deque<Integer> q = new ArrayDeque<>();
-        q.offer(curr);
-        visited.add(curr);
+        q.offer(start);
+        visited.add(start);
         int moves = 1;
         while(!q.isEmpty()) {
             int size = q.size();
             while(size > 0) {
                 int pos = q.poll();
-                // if(pos == target) return moves;
                 for(int next = pos+1; next <= pos+6; next++) {
                     int trueNext = map.getOrDefault(next, next);
                     if(!visited.contains(trueNext)) {
